662bad0d9ba6bb84fcb2aecd8630f97457ec9218
Kyohei Uto <im@kyoheiu.dev>
2023-01-11 05:28:54 +0900

    Sticky header; reload to the same position in index

--- static/index.js
+++ static/index.js
@@ -11,7 +11,7 @@ const fetch_add = () => {
       throw new Error("Cannot add new article.");
     }
     console.log(response.status);
-    window.location.reload();
+    add_element("test");
   });
 };
 
@@ -22,6 +22,26 @@ const fetch_delete = (e) => {
       throw new Error("Cannot add new article.");
     }
     console.log(response.status);
-    window.location.reload();
+    console.log(e.value);
+    remove_element(e.value);
   });
+};
+
+const add_element = (id) => {
+  console.log(id);
+  window.location.reload();
+};
+
+const remove_element = (id) => {
+  const child = document.getElementById(id);
+  Element.innerHTML = "";
+};
+
+document.addEventListener("DOMContentLoaded", function (event) {
+  var scrollpos = localStorage.getItem("scrollpos");
+  if (scrollpos) window.scrollTo(0, scrollpos);
+});
+
+window.onbeforeunload = function (e) {
+  localStorage.setItem("scrollpos", window.scrollY);
 };
