e308c6edfd623df8eeb35e90aa774344ad632426
Kyohei Uto <im@kyoheiu.dev>
2023-01-27 13:48:57 +0900

    Add scroll event

--- pages/articles/[id].tsx
+++ pages/articles/[id].tsx
@@ -1,13 +1,8 @@
-import {
-  WrappedData,
-  ElementKind,
-  ArticleData,
-  ArticleContent,
-} from "../../types/types";
+import { ArticleContent } from "../../types/types";
 import { Header } from "../../components/Header";
 import ArticleElement from "../../components/ArticleElement";
 import useSWR, { Fetcher } from "swr";
-import { useReducer } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import { GetServerSideProps } from "next";
 import { InferGetServerSidePropsType } from "next";
@@ -33,6 +28,45 @@ export const getServerSideProps: GetServerSideProps<{
 export default function Searched({
   data,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  const [pos, setPos] = useState(0);
+  const [prog, setProg] = useState(0);
+
+  const getScrollPosition = () => {
+    const bodyheight = document.documentElement.scrollHeight;
+    const scrolled = document.documentElement.scrollTop;
+    const client = document.documentElement.clientHeight;
+
+    const new_pos = Math.round((scrolled * 100) / bodyheight);
+    const new_prog = Math.abs(bodyheight - client - scrolled);
+    if (prog < 1) {
+      setPos(new_pos);
+      setProg(100);
+      return;
+    } else {
+      setPos(new_pos);
+      setProg(100 - Math.round((new_prog * 100) / bodyheight));
+    }
+  };
+
+  const fetch_pos = () => {
+    const numbers = getScrollPosition();
+    console.log("pos: " + pos + " prog: " + prog);
+    const target =
+      "http://localhost:8000/articles/" +
+      data.id +
+      "?pos=" +
+      pos +
+      "&prog=" +
+      prog;
+    fetch(target, { method: "POST" }).then((res) => {
+      if (!res.ok) {
+        console.error("Cannot update progress.");
+      }
+    });
+  };
+
+  useEffect(() => globalThis.addEventListener("scroll", fetch_pos), []);
+
   if (!data) {
     return <h1>No article found.</h1>;
   }
