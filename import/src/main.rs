994ee4d0b1b0e914d1f16cea092beb780b8a08ab
Kyohei Uto <im@kyoheiu.dev>
2023-02-03 05:26:15 +0900

    Create multiple articles (non-parallel)

--- import/src/main.rs
+++ import/src/main.rs
@@ -3,38 +3,52 @@ use serde::{Deserialize, Serialize};
 
 #[tokio::main]
 async fn main() {
-    let args = std::env::args();
+    let mut args = std::env::args();
+    let mut rdr = csv::Reader::from_path("instapaper-export.csv").unwrap();
     if args.len() == 1 {
-        let mut rdr = csv::Reader::from_path("instapaper-export.csv").unwrap();
         let mut i: usize = 0;
         for result in rdr.deserialize() {
             let article: Article = result.unwrap();
-            println!("{:#?}", article);
-            let req = Req {
-                url: article.url.to_owned(),
-            };
-            let j = serde_json::to_string(&req).unwrap();
-            let mut headers = HeaderMap::new();
-            headers.insert(
-                "Content-Type",
-                header::HeaderValue::from_str("application/json").unwrap(),
-            );
-            let client = reqwest::Client::builder()
-                .default_headers(headers)
-                .build()
-                .unwrap();
-            let _res = client
-                .post("http://localhost:3000/api/create")
-                .body(j)
-                .send()
-                .await
-                .unwrap();
+            send_req(article).await;
             i += 1;
             if i == 1 {
                 break;
             }
         }
+    } else if args.len() == 2 {
+        let number = args.nth(1).unwrap();
+        let limit = number.parse::<usize>().unwrap();
+        for (index, result) in rdr.deserialize().enumerate() {
+            let article: Article = result.unwrap();
+            send_req(article).await;
+            if index == limit {
+                break;
+            }
+        }
     }
+}
+
+async fn send_req(article: Article) {
+    println!("{:#?}", article);
+    let req = Req {
+        url: article.url.to_owned(),
+    };
+    let j = serde_json::to_string(&req).unwrap();
+    let mut headers = HeaderMap::new();
+    headers.insert(
+        "Content-Type",
+        header::HeaderValue::from_str("application/json").unwrap(),
+    );
+    let client = reqwest::Client::builder()
+        .default_headers(headers)
+        .build()
+        .unwrap();
+    let _res = client
+        .post("http://localhost:3000/api/create")
+        .body(j)
+        .send()
+        .await
+        .unwrap();
 }
 
 #[derive(Debug, Serialize, Deserialize)]
