3375a6980976d8d1b63a7efbdd60db6283152945
kyoheiu <kyoheiu@outlook.com>
2023-02-15 05:44:30 +0900

    Add compiler option (remove console)

--- client/next.config.js
+++ client/next.config.js
@@ -4,6 +4,11 @@ const nextConfig = {
   experimental: {
     scrollRestoration: true,
   },
+  compiler: {
+    removeConsole: {
+      exclude: ["error"],
+    },
+  },
 };
 
 module.exports = nextConfig;
