1f4941f96faf8d1420f208b95a5cfe46cee085a2
kyoheiu <kyoheiu@outlook.com>
2023-02-19 07:05:44 +0900

    Fix api path

--- client/pages/api/reload_archived.ts
+++ client/pages/api/reload_archived.ts
@@ -1,6 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { getSession } from "next-auth/react";
-import puppeteer from "puppeteer";
 
 export default async function handler(
   req: NextApiRequest,
@@ -15,7 +14,7 @@ export default async function handler(
     let id: string = req.body;
 
     const response = await fetch(
-      `http://${process.env.NEXT_PUBLIC_HOST}:8000/articles?reload=${id}`
+      `http://${process.env.NEXT_PUBLIC_HOST}:8000/articles/archived?reload=${id}`
     );
     const j = await response.json();
     res.status(200).json(j);
