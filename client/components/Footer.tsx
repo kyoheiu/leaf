4fcf4fc25179b06eb32ed5382982282ad0a0ee11
kyoheiu <kyoheiu@outlook.com>
2023-02-19 07:03:17 +0900

    Use logo in footer

--- client/components/Footer.tsx
+++ client/components/Footer.tsx
@@ -1,7 +1,25 @@
+import Divider from "@mui/material/Divider";
+import Image from "next/image";
+
 interface FooterProps {
   isLast: boolean;
 }
 
 export default function Footer({ isLast }: FooterProps) {
-  return <footer>{isLast ? "End of the list." : ""}</footer>;
+  const logo_size = 28;
+
+  return (
+    <>
+      <footer>
+        {isLast && (
+          <Image
+            src="/logo.png"
+            alt="hmstr"
+            height={logo_size}
+            width={logo_size}
+          />
+        )}
+      </footer>
+    </>
+  );
 }
