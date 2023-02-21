ad8ce3bb3805c579dc137ec8fe0178ed8909672f
kyoheiu <kyoheiu@outlook.com>
2023-02-21 15:17:08 +0900

    Remove puppeteer process from client

--- client/pages/api/create.ts
+++ client/pages/api/create.ts
@@ -1,20 +1,10 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { getSession } from "next-auth/react";
-import puppeteer, { Browser } from "puppeteer";
 
 interface Content {
   url: string;
   html: string;
 }
-
-const crawl = async (url: string, browser: Browser): Promise<string> => {
-  console.log(url);
-  const page = await browser.newPage();
-  await page.goto(url);
-  const text = await page.content();
-  await page.close();
-  return text;
-};
 
 export default async function handler(
   req: NextApiRequest,
@@ -28,17 +18,6 @@ export default async function handler(
     let url: string = req.body;
     console.log(req.body);
 
-    // const browser = await puppeteer.launch({
-    //   headless: true,
-    //   args: [
-    //     "--disable-gpu",
-    //     "--disable-dev-shm-usage",
-    //     "--disable-setuid-sandbox",
-    //     "--no-sandbox",
-    //   ],
-    // });
-    // const html = await crawl(url, browser);
-    // const body = JSON.stringify({ url: url, html: html });
     const response = await fetch(
       `http://${process.env.NEXT_PUBLIC_HOST}:8000/articles`,
       {
