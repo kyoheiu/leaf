4a2bd5acad3cd792d41c13a550e4f8606d0b944f
Kyohei Uto <im@kyoheiu.dev>
2023-02-26 06:38:45 +0900

    Use simple form instead of dialog

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -1,20 +1,12 @@
 import Link from "next/link";
 import { useContext, useState } from "react";
-import Dialog from "@mui/material/Dialog";
-import DialogActions from "@mui/material/DialogActions";
-import DialogTitle from "@mui/material/DialogTitle";
-import DialogContent from "@mui/material/DialogContent";
 import TextField from "@mui/material/TextField";
-import Button from "@mui/material/Button";
 import { Link as MuiLink } from "@mui/material";
 import Grid from "@mui/material/Grid";
-import SearchIcon from "@mui/icons-material/Search";
-import Add from "@mui/icons-material/Add";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import ArchiveIcon from "@mui/icons-material/Archive";
 import LightModeIcon from "@mui/icons-material/LightMode";
 import DarkModeIcon from "@mui/icons-material/DarkMode";
-import CloseIcon from "@mui/icons-material/Close";
 import LogoutIcon from "@mui/icons-material/Logout";
 import { ColorMode } from "../context/ColorMode";
 import Image from "next/image";
@@ -26,11 +18,12 @@ export const Header = () => {
 
   const logo_width = 1.5;
   const button_width = 0.8;
-  const blank_space = 12 - logo_width - button_width * 6;
+  const input_width = (12 - logo_width - button_width * 4) / 2;
   const logo_size = 28;
   const button_size = 18;
 
   const [url, setUrl] = useState<string>("");
+  const [query, setQuery] = useState<string>("");
   const { isLight, setIsLight } = useContext(ColorMode);
 
   const toggle_theme = () => {
@@ -43,33 +36,13 @@ export const Header = () => {
     });
   };
 
-  const [addOpen, setAddOpen] = useState(false);
-  const [searchOpen, setSearchOpen] = useState(false);
-
-  const handleClickAddOpen = () => {
-    setAddOpen(true);
-  };
-
-  const handleAddClose = () => {
-    setAddOpen(false);
-  };
-
-  const handleClickSearchOpen = () => {
-    setSearchOpen(true);
-  };
-
-  const handleSearchClose = () => {
-    setSearchOpen(false);
-  };
-
-  const submitAndClose = async () => {
-    const url = (document.getElementById("add_new_article") as HTMLInputElement)
-      .value;
-    const res = await fetch("/api/create", {
+  const createNew = async (e) => {
+    e.preventDefault();
+    console.log(url);
+    const res = await fetch("/api/articles", {
       method: "POST",
       body: url,
     });
-    setAddOpen(false);
     if (!res.ok) {
       console.log("Cannot create new article.");
     } else {
@@ -78,11 +51,8 @@ export const Header = () => {
     }
   };
 
-  const searchAndClose = async () => {
-    const queries: string = (
-      document.getElementById("search") as HTMLInputElement
-    ).value;
-    const split = queries
+  const search = async (e) => {
+    const split = query
       .split(/(\s+)/)
       .filter((x) => x.trim().length > 0)
       .join("+");
@@ -107,61 +77,32 @@ export const Header = () => {
             />
           </MuiLink>
         </Grid>
-        <Grid item xs={button_width}>
-          <MuiLink href="#" onClick={handleClickAddOpen}>
-            <Add sx={{ fontSize: button_size }} />
-          </MuiLink>
-          <Dialog open={addOpen} onClose={handleAddClose}>
-            <DialogTitle>Add new article.</DialogTitle>
-            <DialogContent>
-              <TextField
-                autoFocus
-                margin="dense"
-                id={"add_new_article"}
-                label="URL to add"
-                type="text"
-                fullWidth
-                variant="standard"
-              />
-            </DialogContent>
-            <DialogActions>
-              <Button onClick={handleAddClose}>
-                <CloseIcon />
-              </Button>
-              <Button onClick={submitAndClose}>
-                <Add />
-              </Button>
-            </DialogActions>
-          </Dialog>
+        <Grid item xs={input_width}>
+          <form onSubmit={createNew}>
+            <TextField
+              id={"add_new"}
+              type="url"
+              value={url}
+              onChange={(e) => setUrl(() => e.target.value)}
+              placeholder="+"
+              size="small"
+              variant="standard"
+            />
+          </form>
         </Grid>
-        <Grid item xs={button_width}>
-          <MuiLink href="#" onClick={handleClickSearchOpen}>
-            <SearchIcon sx={{ fontSize: button_size }} />
-          </MuiLink>
-          <Dialog open={searchOpen} onClose={handleSearchClose}>
-            <DialogTitle>Search.</DialogTitle>
-            <DialogContent>
-              <TextField
-                autoFocus
-                margin="dense"
-                id={"search"}
-                label=""
-                type="text"
-                fullWidth
-                variant="standard"
-              />
-            </DialogContent>
-            <DialogActions>
-              <Button onClick={handleSearchClose}>
-                <CloseIcon />
-              </Button>
-              <Button onClick={searchAndClose}>
-                <SearchIcon />
-              </Button>
-            </DialogActions>
-          </Dialog>
+        <Grid item xs={input_width}>
+          <form onSubmit={search}>
+            <TextField
+              id={"search"}
+              type="text"
+              value={query}
+              onChange={(e) => setQuery(() => e.target.value)}
+              placeholder="search"
+              size="small"
+              variant="standard"
+            />
+          </form>
         </Grid>
-        <Grid item xs={blank_space} />
         <Grid item xs={button_width}>
           <MuiLink component={Link} href="/liked">
             <FavoriteIcon sx={{ fontSize: button_size }} />
