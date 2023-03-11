c51da1fde92c493bda1f6994ce3c51bb64292287
Kyohei Uto <im@kyoheiu.dev>
2023-03-12 06:10:26 +0900

    Add /api/create with token auth

--- client/pages/api/create.ts
+++ client/pages/api/create.ts
@@ -2,19 +2,21 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { createArticle } from "./articles";
 
 export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse,
+	req: NextApiRequest,
+	res: NextApiResponse,
 ) {
-  if (req.method === "POST") {
-    let url: string = req.body;
-    const response = await createArticle(url);
-    if (!response.ok) {
-      res.send(response.body);
-    } else {
-      res.status(200).end();
-    }
-  } else {
-    res.status(404).end();
-  }
+	if (req.method === "POST") {
+		if (req.headers.authorization !== process.env.WEB_API_TOKEN) {
+			res.status(404).end();
+		} else {
+			const response = await createArticle(req.body.url);
+			if (!response.ok) {
+				res.send(response.body);
+			} else {
+				res.status(200).end();
+			}
+		}
+	} else {
+		res.status(404).end();
+	}
 }
-
