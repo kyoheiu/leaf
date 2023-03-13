072bfd46105d53491ce0972bcd9649708d313475
Kyohei Uto <im@kyoheiu.dev>
2023-03-14 06:03:27 +0900

    Open api/create (experimental)

--- client/pages/api/create.ts
+++ client/pages/api/create.ts
@@ -6,17 +6,11 @@ export default async function handler(
 	res: NextApiResponse,
 ) {
 	if (req.method === "POST") {
-		if (req.headers.authorization !== process.env.WEB_API_TOKEN) {
-			res.status(404).end();
+		const response = await createArticle(req.body.url);
+		if (!response.ok) {
+			res.send(response.body);
 		} else {
-			const response = await createArticle(req.body.url);
-			if (!response.ok) {
-				res.send(response.body);
-			} else {
-				res.status(200).end();
-			}
+			res.status(200).end();
 		}
-	} else {
-		res.status(404).end();
 	}
 }
