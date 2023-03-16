68aa4cdb0bc7b4195b89db7bebd8ec25026d322f
Kyohei Uto <im@kyoheiu.dev>
2023-03-17 05:23:11 +0900

    Remove next-pwa

--- client/next.config.js
+++ client/next.config.js
@@ -1,20 +1,13 @@
 /** @type {import('next').NextConfig} */
 
-const withPWA = require("next-pwa")({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  // disable: process.env.NEXT_PUBLIC_NODE_ENV === "development",
-});
-
-module.exports = withPWA({
-  reactStrictMode: true,
-  experimental: {
-    scrollRestoration: true,
-  },
-  compiler: {
-    removeConsole: {
-      exclude: ["error"],
-    },
-  },
-});
+module.exports = {
+	reactStrictMode: true,
+	experimental: {
+		scrollRestoration: true,
+	},
+	compiler: {
+		removeConsole: {
+			exclude: ["error"],
+		},
+	},
+};
