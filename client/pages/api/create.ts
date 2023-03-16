fe0835f0287916b798fe2c4da1a2b45090b5d5e2
Kyohei Uto <im@kyoheiu.dev>
2023-03-16 16:14:53 +0900

    Add res.header

--- client/pages/api/create.ts
+++ client/pages/api/create.ts
@@ -2,20 +2,22 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { createArticle } from "./articles";
 
 export default async function handler(
-	req: NextApiRequest,
-	res: NextApiResponse,
+  req: NextApiRequest,
+  res: NextApiResponse,
 ) {
-	if (req.method === "POST") {
-		const query = req.query;
-		if (query.url) {
-			const response = await createArticle(query.url as string);
-			if (!response.ok) {
-				res.send(response.body);
-			} else {
-				res.status(200).end();
-			}
-		}
-	} else {
-		res.status(404).end();
-	}
+  if (req.method === "POST") {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Method", "POST");
+    const query = req.query;
+    if (query.url) {
+      const response = await createArticle(query.url as string);
+      if (!response.ok) {
+        res.send(response.body);
+      } else {
+        res.status(200).end();
+      }
+    }
+  } else {
+    res.status(404).end();
+  }
 }
