33c628aa281c449450aec237ee7c3ee0f00af317
Kyohei Uto <im@kyoheiu.dev>
2023-03-17 09:28:32 +0900

    Firefox extension MVP

--- extension/background.js
+++ extension/background.js
@@ -1,20 +1,20 @@
 const addNewOne = (tab) => {
-  const gettingItem = browser.storage.local.get(["hmstrBase", "hmstrToken"]);
-  gettingItem.then(res => {
-    const target = res.hmstrBase + "/api/create";
-    console.error(target)
-    fetch(target, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: res.token
-      },
-      body: JSON.stringify({url: tab.url})
-    }).then(res => {
-      console.error(res.statusText)
-    })
-  })
-}
+	const gettingItem = browser.storage.local.get(["hmstrBase", "hmstrToken"]);
+	gettingItem.then((res) => {
+		const target = `${res.hmstrBase}/api/create`;
+		console.log(`Add new article: ${tab.url}`);
 
-console.log("Hi there.");
+		fetch(target, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: res.token,
+			},
+			body: JSON.stringify({ url: tab.url }),
+		}).then((res) => {
+			console.log(res.statusText);
+		});
+	});
+};
+
 browser.action.onClicked.addListener((tab) => addNewOne(tab));
