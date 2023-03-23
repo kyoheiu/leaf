35d3d9c39948e7e860459b9e7fbe08ef685e0098
Kyohei Uto <im@kyoheiu.dev>
2023-03-24 05:33:08 +0900

    Fix: Add cleaner

--- client/pages/articles/[id].tsx
+++ client/pages/articles/[id].tsx
@@ -1,7 +1,6 @@
 import { ArticleContent } from "../../types/types";
 import { useEffect, useState } from "react";
 import { GetServerSideProps, InferGetServerSidePropsType } from "next";
-import { useRouter } from "next/router";
 import Button from "@mui/material/Button";
 import Divider from "@mui/material/Divider";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
@@ -34,10 +33,7 @@ export default function Article({
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
 	const { data: session, status } = useSession();
 
-	const router = useRouter();
 	const [articleContent, setArticleContent] = useState<ArticleContent>(data);
-
-	const [shouldSaveScroll, setShouldSaveScroll] = useState(true);
 
 	const getScrollPosition = () => {
 		const bodyheight = document.documentElement.scrollHeight;
@@ -53,20 +49,18 @@ export default function Article({
 	};
 
 	const saveScrollPos = () => {
-		if (shouldSaveScroll) {
-			const n = getScrollPosition();
-			if (n.pos !== 0) {
-				fetch(
-					`/api/articles/${articleContent.id}?pos=${n.pos}&prog=${n.prog}`,
-					{
-						method: "POST",
-					},
-				).then((res) => {
-					if (!res.ok) {
-						console.error("Cannot update progress.");
-					}
-				});
-			}
+		const n = getScrollPosition();
+		if (n.pos !== 0) {
+			fetch(
+				`/api/articles/${articleContent.id}?pos=${n.pos}&prog=${n.prog}`,
+				{
+					method: "POST",
+				},
+			).then((res) => {
+				if (!res.ok) {
+					console.error("Cannot update progress.");
+				}
+			});
 		}
 	};
 
@@ -79,27 +73,16 @@ export default function Article({
 	};
 
 	useEffect(() => {
-		restoreScrollPos();
+		let mounted = true;
+		if (mounted) {
+			restoreScrollPos();
+		}
+		return () => { mounted = false };
 	}, []);
 
 	useEffect(() => {
 		globalThis.addEventListener("scroll", saveScrollPos);
 		return () => globalThis.removeEventListener("scroll", saveScrollPos);
-	}, []);
-
-	useEffect(() => {
-		const setSaveOff = () => {
-			setShouldSaveScroll((_v) => false);
-		};
-		router.beforePopState(() => {
-			setShouldSaveScroll(() => false);
-			console.log(shouldSaveScroll);
-			return true;
-		});
-		router.events.on("routeChangeStart", setSaveOff);
-		return () => {
-			router.events.off("routeChangeStart", setSaveOff);
-		};
 	}, []);
 
 	const toggleLiked = async () => {
@@ -197,6 +180,7 @@ export default function Article({
 				<title>
 					{articleContent.title} | {process.env.NEXT_PUBLIC_TITLE}
 				</title>
+				<script src="/readability.js" />
 			</Head>
 			<div className="article-title">{articleContent.title}</div>
 			<div className="article-url">
