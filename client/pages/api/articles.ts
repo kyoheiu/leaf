7b1062c612f151f23797853dc796b1359834b8ca
Kyohei Uto <im@kyoheiu.dev>
2023-03-25 16:38:24 +0900

    Use rust_headless_chrome in the proper flow

--- client/pages/api/articles.ts
+++ client/pages/api/articles.ts
@@ -3,14 +3,6 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import puppeteer, { Browser } from "puppeteer";
 import jsdom from "jsdom";
 
-interface Content {
-	url: string;
-	html: string;
-	title: string;
-	plain: string;
-	og: string | null | undefined;
-}
-
 export const getArticles = async () => {
 	const response = await fetch(
 		`http://${process.env.NEXT_PUBLIC_HOST}:8000/articles`,
@@ -28,46 +20,9 @@ const reloadArticles = async (id: string) => {
 };
 
 export const createArticle = async (url: string) => {
-	const { JSDOM } = jsdom;
-
-	const browser = await puppeteer.launch({
-		executablePath: "chromium",
-		headless: true,
-		args: [
-			"--disable-gpu",
-			"--disable-dev-shm-usage",
-			"--disable-setuid-sandbox",
-			"--no-sandbox",
-		],
-	});
-	const crawled = await crawl(url, browser);
-	await browser.close();
-
-	const dom = new JSDOM(crawled, { url: url });
-	const document = dom.window.document;
-	const parsed = new Readability(document).parse();
-	if (!parsed) {
-		throw Error("Cannot parse document.");
-	}
-
-	const og = document
-		.querySelector("[property='og:image']")
-		?.getAttribute("content");
-
-	const content: Content = {
-		url: url,
-		title: parsed.title ?? "",
-		html: parsed.content ?? "",
-		plain: parsed.textContent ?? "",
-		og: og,
-	};
-	const body = JSON.stringify(content);
 	return await fetch(`http://${process.env.NEXT_PUBLIC_HOST}:8000/articles`, {
 		method: "POST",
-		headers: {
-			"Content-Type": "application/json",
-		},
-		body: body,
+		body: url,
 	});
 };
 
