59a849ea197162f6d48690c199998e18eeeb3796
Kyohei Uto <im@kyoheiu.dev>
2023-04-26 11:58:34 +0900

    Remove unused functions

--- client/components/Footer.tsx
+++ client/components/Footer.tsx
@@ -1,6 +1,4 @@
 import Image from "next/image";
-import SyncIcon from "@mui/icons-material/Sync";
-import Button from "@mui/material/Button";
 import { LOGO_SIZE } from "./Header";
 import { useContext } from "react";
 import { ColorMode } from "../context/ColorMode";
@@ -13,19 +11,3 @@ export const footerImage = () => {
 			<Image src="/logo_dark.png" alt="leaf" height={LOGO_SIZE} width={LOGO_SIZE} />
 	);
 };
-
-const syncButton = (reload: () => Promise<void>) => {
-	return (
-		<Button variant="outlined" fullWidth={true} onClick={reload}>
-			<SyncIcon />
-		</Button>
-	);
-};
-
-export const Footer = (isLast: boolean, reload: () => Promise<void>) => {
-	return <footer>{isLast ? footerImage() : syncButton(reload)}</footer>;
-};
-
-export const SearchFooter = () => {
-	return (<footer>{footerImage()}</footer>);
-}
