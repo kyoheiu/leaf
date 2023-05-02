cc4ac81cdd46795798b5614699fac49272169bc6
Kyohei Uto <im@kyoheiu.dev>
2023-05-02 14:37:48 +0900

    Use once_cell instead of lazy_static

--- server/src/readable.rs
+++ server/src/readable.rs
@@ -3,35 +3,35 @@
 
 use html5ever::tendril;
 use html5ever::tendril::StrTendril;
-use lazy_static::lazy_static;
 use nipper::Document;
 use nipper::Selection;
 use regex::Regex;
 use std::collections::HashMap;
 use std::ops::Deref;
+use once_cell::sync::Lazy;
+use colored::Colorize;
 
 use crate::error::Error;
-lazy_static! {
-    static ref RE_REPLACE_BRS: Regex = Regex::new(r#"(?i)(<br[^>]*>[ \n\r\t]*){2,}"#).unwrap();
-    static ref RE_TITLE_SEPARATOR: Regex = Regex::new(r#"(?i) [\|\-\\/>»] "#).unwrap();
-    static ref RE_TITLE_HIERARCHY_SEP: Regex = Regex::new(r#"(?i)[\\/>»]"#).unwrap();
-    static ref RE_BYLINE: Regex = Regex::new(r#"(?i)byline|author|dateline|writtenby|p-author"#).unwrap();
-    static ref RE_UNLIKELY_CANDIDATES: Regex = Regex::new(r#"(?i)-ad-|ai2html|banner|breadcrumbs|combx|comment|community|cover-wrap|disqus|extra|footer|gdpr|header|legends|menu|related|remark|replies|rss|shoutbox|sidebar|skyscraper|social|sponsor|supplemental|ad-break|agegate|pagination|pager|popup|yom-remote"#).unwrap();
-    static ref RE_OK_MAYBE_CANDIDATE: Regex = Regex::new(r#"(?i)and|article|body|column|content|main|shadow"#).unwrap();
-    static ref RE_UNLIKELY_ROLES: Regex = Regex::new(r#"(?i)menu|menubar|complementary|navigation|alert|alertdialog|dialog"#).unwrap();
-    static ref RE_POSITIVE: Regex = Regex::new(r#"(?i)article|body|content|entry|hentry|h-entry|main|page|pagination|post|text|blog|story"#).unwrap();
-    static ref RE_NEGATIVE: Regex = Regex::new(r#"-ad-|hidden|^hid$| hid$| hid |^hid |banner|combx|comment|com-|contact|foot|footer|footnote|gdpr|masthead|media|meta|outbrain|promo|related|scroll|share|shoutbox|sidebar|skyscraper|sponsor|shopping|tags|tool|widget"#).unwrap();
-    static ref RE_DIV_TO_P_ELEMENTS: Regex = Regex::new(r#"(?i)<(a|blockquote|dl|div|img|ol|p|pre|table|ul)"#).unwrap();
-    static ref RE_VIDEOS: Regex = Regex::new(r#"(?i)//(www\.)?(dailymotion|youtube|youtube-nocookie|player\.vimeo)\.com"#).unwrap();
-    static ref RE_P_IS_SENTENCE: Regex = Regex::new(r#"(?i)\.( |$)"#).unwrap();
-    static ref RE_COMMENTS: Regex = Regex::new(r#"(?i)<!--[^>]+-->"#).unwrap();
-    static ref RE_KILL_BREAKS: Regex = Regex::new(r#"(?i)(<br\s*/?>(\s|&nbsp;?)*)+"#).unwrap();
-    static ref RE_SPACES: Regex = Regex::new(r#"(?i)\s{2,}|\n+"#).unwrap();
-    static ref RE_HASHURL: Regex = Regex::new(r#"^#.+"#).unwrap();
-    static ref RE_SHARE_ELEMENT: Regex = Regex::new(r#"(?i)(\b|_)(share|sharedaddy)(\b|_)"#).unwrap();
-    static ref RE_PHRASING_ELEMS: Regex = Regex::new(r#"(?i)^(abbr|audio|b|bdo|br|button|cite|code|data|datalist|dfn
-    em|embed|i|img|input|kbd|label|mark|math|meter|noscript|object|output|progress|q|ruby|samp|script|select|small|span|strong|sub|sup|textarea|time|var|wbr)$"#).unwrap();
-}
+
+    static RE_REPLACE_BRS: Lazy<Regex> = Lazy::new(|| Regex::new(r#"(?i)(<br[^>]*>[ \n\r\t]*){2,}"#).unwrap());
+    static RE_TITLE_SEPARATOR: Lazy<Regex> =Lazy::new(||  Regex::new(r#"(?i) [\|\-\\/>»] "#).unwrap());
+    static RE_TITLE_HIERARCHY_SEP:Lazy<Regex> =Lazy::new(||  Regex::new(r#"(?i)[\\/>»]"#).unwrap());
+    static RE_BYLINE:Lazy<Regex> =Lazy::new(||  Regex::new(r#"(?i)byline|author|dateline|writtenby|p-author"#).unwrap());
+    static RE_UNLIKELY_CANDIDATES:Lazy<Regex> =Lazy::new(||  Regex::new(r#"(?i)-ad-|ai2html|banner|breadcrumbs|combx|comment|community|cover-wrap|disqus|extra|footer|gdpr|header|legends|menu|related|remark|replies|rss|shoutbox|sidebar|skyscraper|social|sponsor|supplemental|ad-break|agegate|pagination|pager|popup|yom-remote"#).unwrap());
+    static RE_OK_MAYBE_CANDIDATE:Lazy<Regex> =Lazy::new(||  Regex::new(r#"(?i)and|article|body|column|content|main|shadow"#).unwrap());
+    static RE_UNLIKELY_ROLES:Lazy<Regex> =Lazy::new(||  Regex::new(r#"(?i)menu|menubar|complementary|navigation|alert|alertdialog|dialog"#).unwrap());
+    static RE_POSITIVE:Lazy<Regex> =Lazy::new(||  Regex::new(r#"(?i)article|body|content|entry|hentry|h-entry|main|page|pagination|post|text|blog|story"#).unwrap());
+    static RE_NEGATIVE:Lazy<Regex> =Lazy::new(||  Regex::new(r#"-ad-|hidden|^hid$| hid$| hid |^hid |banner|combx|comment|com-|contact|foot|footer|footnote|gdpr|masthead|media|meta|outbrain|promo|related|scroll|share|shoutbox|sidebar|skyscraper|sponsor|shopping|tags|tool|widget"#).unwrap());
+    static RE_DIV_TO_P_ELEMENTS:Lazy<Regex> =Lazy::new(||  Regex::new(r#"(?i)<(a|blockquote|dl|div|img|ol|p|pre|table|ul)"#).unwrap());
+    static RE_VIDEOS:Lazy<Regex> =Lazy::new(||  Regex::new(r#"(?i)//(www\.)?(dailymotion|youtube|youtube-nocookie|player\.vimeo)\.com"#).unwrap());
+    static RE_P_IS_SENTENCE:Lazy<Regex> =Lazy::new(||  Regex::new(r#"(?i)\.( |$)"#).unwrap());
+    static RE_COMMENTS:Lazy<Regex> =Lazy::new(||  Regex::new(r#"(?i)<!--[^>]+-->"#).unwrap());
+    static RE_KILL_BREAKS:Lazy<Regex> =Lazy::new(||  Regex::new(r#"(?i)(<br\s*/?>(\s|&nbsp;?)*)+"#).unwrap());
+    static RE_SPACES:Lazy<Regex> =Lazy::new(||  Regex::new(r#"(?i)\s{2,}|\n+"#).unwrap());
+    static RE_HASHURL:Lazy<Regex> =Lazy::new(||  Regex::new(r#"^#.+"#).unwrap());
+    static RE_SHARE_ELEMENT:Lazy<Regex> =Lazy::new(||  Regex::new(r#"(?i)(\b|_)(share|sharedaddy)(\b|_)"#).unwrap());
+    static RE_PHRASING_ELEMS:Lazy<Regex> =Lazy::new(||  Regex::new(r#"(?i)^(abbr|audio|b|bdo|br|button|cite|code|data|datalist|dfn
+    em|embed|i|img|input|kbd|label|mark|math|meter|noscript|object|output|progress|q|ruby|samp|script|select|small|span|strong|sub|sup|textarea|time|var|wbr)$"#).unwrap());
 
 const DATA_TABLE_ATTR: &str = "XXX-DATA-TABLE";
 
@@ -529,7 +529,7 @@ fn grab_article(doc: &Document, title: &str) -> String {
                 sel.replace_with_selection(&node);
                 elements_to_score.push(sel);
             } else if !has_child_block_element(&sel) {
-                set_node_tag!(&mut sel, "p");
+                set_node_tag!(sel, "p");
                 elements_to_score.push(sel);
             }
         }
@@ -646,6 +646,9 @@ fn grab_article(doc: &Document, title: &str) -> String {
     let new_doc = Document::from(r#""#);
     let mut content = new_doc.select("body");
 
+    // Now that we have the top candidate, look through its siblings for content
+    // that might also be related. Things like preambles, content split by ads
+    // that we removed, etc.
     let top_selection = &top_candidate.sel;
     top_selection
         .parent()
@@ -671,7 +674,7 @@ fn grab_article(doc: &Document, title: &str) -> String {
                         if sibling.is("p") {
                             let link_density = get_link_density(&sibling);
                             let node_content = sibling.text();
-                            let node_length = sibling.length();
+                            let node_length = node_content.len();
 
                             if node_length > 80 && link_density < 0.25 {
                                 true
