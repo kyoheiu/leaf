f24dd499dc613fb74f1b4a4aa89ca740b9f64a61
Kyohei Uto <im@kyoheiu.dev>
2023-08-27 15:42:08 +0900

    Handle empty query

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -18,21 +18,23 @@ export const Header = () => {
   const [searchOpen, setSearchOpen] = useState(false);
   const [loading, setLoading] = useState(false);
 
-  const handleClickSearchOpen = () => {
+  const handleClickSearchToggle = () => {
     setSearchOpen((b) => !b);
   };
 
-  const execSearch = async () => {
-    const queries = (document.getElementById("search") as HTMLInputElement)
-      .value;
-    const split = queries
-      .split(/(\s+)/)
-      .filter((x) => x.trim().length > 0)
-      .join("+");
-    router.push(`/search?q=${split}`);
+  const execSearch = async (e: React.FormEvent) => {
+    e.preventDefault();
+    if (query.trim().length === 0) {
+      return;
+    } else {
+      const q = query.split(/(\s+)/).filter((x) => x.trim().length > 0)[0];
+      setSearchOpen((b) => !b);
+      router.push(`/search?q=${q}`);
+    }
   };
 
   const [url, setUrl] = useState<string>("");
+  const [query, setQuery] = useState<string>("");
 
   const createNew = async (e: React.FormEvent): Promise<void | string> => {
     setLoading(() => true);
@@ -56,7 +58,7 @@ export const Header = () => {
       <>
         <button
           className="ml-auto"
-          onClick={handleClickSearchOpen}
+          onClick={handleClickSearchToggle}
           title="search"
         >
           <RiSearch2Line />
@@ -70,7 +72,7 @@ export const Header = () => {
       <Head>
         <title>leaf</title>
       </Head>
-      <div className="mb-6 mt-3 flex flex-nowrap items-center justify-between">
+      <div className="mt-3 flex flex-nowrap items-center justify-between">
         <Link className="pr-1" href="/">
           <Image
             src="/logo.png"
@@ -98,19 +100,21 @@ export const Header = () => {
         )}
         <SearchButton />
         <div className="ml-3">
-          <a href="https://git.sr.ht/~kyoheiu/leaf" target="_blank">
+          <a href="https://github.com/kyoheiu/leaf" target="_blank">
             <RiCodeSSlashFill />
           </a>
         </div>
       </div>
       {searchOpen && (
-        <form onSubmit={execSearch} className="flex justify-end">
+        <form onSubmit={(e) => execSearch(e)} className="mt-3 flex justify-end">
           <input
             id="search"
             type="text"
+            value={query}
             placeholder="search"
             className="mb-2 w-3/5 rounded-md border border-slate-500 p-1 text-sm text-gray-900"
             autoFocus
+            onChange={(e) => setQuery(() => e.target.value)}
           />
         </form>
       )}
