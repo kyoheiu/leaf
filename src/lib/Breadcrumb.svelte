513cfac1737144602644b415ebf67a5b76fc0e2e
Kyohei Uto <im@kyoheiu.dev>
2023-09-15 14:45:45 +0900

    Fix style

--- src/lib/Breadcrumb.svelte
+++ src/lib/Breadcrumb.svelte
@@ -4,9 +4,7 @@
 	export let page: string;
 </script>
 
-<div
-	class="w-56 mt-6 flex items-center justify-center rounded bg-slate-700 px-6 py-1 text-slate-50"
->
+<div class="w-56 mt-6 flex items-center border border-bordercolor justify-center rounded px-6 py-1">
 	<a class="w-16" href="/">Home</a>
 	<CaretRight size={16} class="mx-4" />
 	<span class="w-16 line-clamp-2 leading-5 text-center">{page}</span>
--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { page } from '$app/stores';
-	import { ArrowCircleRight, ArrowCircleLeft } from 'phosphor-svelte';
+	import { CaretCircleRight, CaretCircleLeft } from 'phosphor-svelte';
 
 	const ICON_SIZE = 32;
 
@@ -11,9 +11,9 @@
 
 <div class="flex space-x-16 my-8">
 	{#if prev}
-		<a href={`${pathname}?page=${prev}`}><ArrowCircleLeft size={ICON_SIZE} /></a>
+		<a href={`${pathname}?page=${prev}`}><CaretCircleLeft size={ICON_SIZE} /></a>
 	{/if}
 	{#if next}
-		<a href={`${pathname}?page=${next}`}><ArrowCircleRight size={ICON_SIZE} /></a>
+		<a href={`${pathname}?page=${next}`}><CaretCircleRight size={ICON_SIZE} /></a>
 	{/if}
 </div>
