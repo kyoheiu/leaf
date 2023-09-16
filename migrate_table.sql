d5cd917f15e2518e8e3c9023c8161abad608eee1
Kyohei Uto <im@kyoheiu.dev>
2023-09-16 10:58:33 +0900

    Fix script to migrate

--- migrate_table.sql
+++ migrate_table.sql
@@ -1,16 +1,16 @@
-CREATE TABLE 'temp' ('ulid', 'tag');
-INSERT INTO 'temp'
-SELECT "ulid",
-    "tag"
-FROM 'tags';
-DROP TABLE 'tags';
-CREATE TABLE 'tags' (
-    'id' INTEGER primary key,
-    'ulid' TEXT NOT NULL,
-    'tag' TEXT NOT NULL
+CREATE TABLE temp (
+    ulid TEXT PRIMARY KEY,
+    tag TEXT
 );
-INSERT INTO 'tags'("ulid", "tag")
-SELECT "ulid",
-    "tag"
-FROM 'temp';
-DROP TABLE 'temp';
+INSERT INTO temp
+SELECT ulid, tag FROM tags;
+DROP TABLE tags;
+CREATE TABLE tags (
+    id INTEGER PRIMARY KEY,
+    ulid TEXT NOT NULL,
+    tag TEXT NOT NULL
+);
+INSERT INTO tags (ulid, tag)
+SELECT ulid, tag
+FROM temp;
+DROP TABLE temp;
