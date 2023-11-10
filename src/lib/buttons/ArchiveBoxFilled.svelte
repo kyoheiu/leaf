48b8a6cefc8925bfcebe89bfc4d308e3d93dab4a
Kyohei Uto <im@kyoheiu.dev>
2023-11-11 05:53:33 +0900

    Add scale transition to buttons

---
+++ src/lib/buttons/ArchiveBoxFilled.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+	import { scale } from 'svelte/transition';
+	import { ICON_SIZE } from './const';
+</script>
+
+<img in:scale src="/archive-box-fill.svg" alt="toggke liked" width={ICON_SIZE} />
---
+++ src/lib/buttons/Trash.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	import { ICON_SIZE } from './const';
+</script>
+
+<img src="/trash.svg" alt="toggke liked" width={ICON_SIZE} />
