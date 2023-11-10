9eed9cb44143630df91d103eef8dcfd9b6cf876b
Kyohei Uto <im@kyoheiu.dev>
2023-11-11 05:53:45 +0900

    Fix link button size

--- src/lib/LinkButton.svelte
+++ src/lib/LinkButton.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 	import { LinkSimple } from 'phosphor-svelte';
 	import { toastSuccess } from '$lib/toast';
+
 	export let url: string;
+	export let size: number;
 
 	const copyToClipboard = async (url: string) => {
 		await navigator.clipboard.writeText(url);
@@ -10,5 +12,5 @@
 </script>
 
 <button on:click={() => copyToClipboard(url)} title="copy URL">
-	<LinkSimple size={18} />
+	<LinkSimple {size} />
 </button>
