428d1a019039c4fbe1a9c0b4002c7b402110da19
Kyohei Uto <im@kyoheiu.dev>
2023-11-11 05:53:03 +0900

    Add viewTransition

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,19 @@
 <script>
 	import { Toaster } from 'svelte-french-toast';
 	import '../app.css';
+
+	import { onNavigate } from '$app/navigation';
+
+	onNavigate((navigation) => {
+		if (!document.startViewTransition) return;
+
+		return new Promise((resolve) => {
+			document.startViewTransition(async () => {
+				resolve();
+				await navigation.complete;
+			});
+		});
+	});
 </script>
 
 <Toaster />
