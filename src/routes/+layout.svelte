f0300384737f9ca9aaaa5e4c1f2514e5fe941fed
Kyohei Uto <im@kyoheiu.dev>
2023-11-12 04:56:16 +0900

    Remove viewTransition

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,19 +1,6 @@
 <script>
 	import { Toaster } from 'svelte-french-toast';
 	import '../app.css';
-
-	import { onNavigate } from '$app/navigation';
-
-	onNavigate((navigation) => {
-		if (!document.startViewTransition) return;
-
-		return new Promise((resolve) => {
-			document.startViewTransition(async () => {
-				resolve();
-				await navigation.complete;
-			});
-		});
-	});
 </script>
 
 <Toaster />
